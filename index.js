805ed4c48469990d31c6000ad89a7bc7f93049a7
Kelvin <vokemalosh32@gmail.com>
2022-06-12 10:03:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Write your code here!
-const newHeader = document.getElementById("victory");
-document.querySelector("#main").remove()
+// const newHeader = document.getElementById("victory");
+// document.querySelector("#main").remove()
+
+function removeMain() {
+    const main = document.querySelector("#main").remove()
+  }
+  
+  removeMain();
+  
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  
+  newHeader.innerHTML = "Kelvin is the champion";
